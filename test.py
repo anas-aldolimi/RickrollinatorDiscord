392f48e99805ba875a6c0d29c70383f275cf10ad
Aldo <54489548+anas-aldolimi@users.noreply.github.com>
2025-05-08 17:15:48 +0300

    Delete test.py

--- test.py
+++
@@ -1,8 +0,0 @@
-with open("Never.txt", 'r') as file:
-    # Read the lines of the file
-    file_lines = file.readlines()
- 
-    # Print each line
-    print("File Content:")
-    for line in file_lines:
-        print(line.strip())
